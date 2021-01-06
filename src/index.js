050dc33cde99be14ca37a82c38952948dbe8eb3c
Brian Frølund <brian.froelund@lego.com>
2021-01-06 20:41:27 +0100

    publish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import Fuse from "fuse.js";
+import 'core-js';
+import 'regenerator-runtime/runtime';
 
-const horseDbUrl = "https://equinet.seges.dk/ords/prod/hdtxt.";
+import Fuse from 'fuse.js';
+
+const horseDbUrl = 'https://equinet.seges.dk/ords/prod/hdtxt.';
 export const searchByIdentity = (registrationNumber) => {
   return fetch(`${horseDbUrl}soeg_ident?sident=*${registrationNumber}*`)
     .then((response) => response.text())
@@ -18,10 +21,7 @@ export const getParantageByRegistrationNumber = (registrationNumber) => {
     if (results.length === 1) {
       return getParantageByHid(results[0].hid);
     }
-    console.debug(
-      "Did not find exactly 1 match when searching by registration number. Results:",
-      results
-    );
+    console.debug('Did not find exactly 1 match when searching by registration number. Results:', results);
     return null;
   });
 };
@@ -31,60 +31,54 @@ export const parseParentageResult = (text) => {
     return {};
   }
 
-  const parseHorse = (horses, filter = "") => {
+  const parseHorse = (horses, filter = '') => {
     const [block] = horses;
     if (!block) {
       return null;
     }
     const resultHorse = { additionalRegistrationNumbers: [] };
-    block.split("\n").forEach((line) => {
-      if (line.startsWith("PRIMÆR_IDENT:")) {
-        resultHorse["registrationNumber"] = line.substring(13);
-      } else if (line.startsWith("FØDSELSDATO:")) {
+    block.split('\n').forEach((line) => {
+      if (line.startsWith('PRIMÆR_IDENT:')) {
+        resultHorse['registrationNumber'] = line.substring(13);
+      } else if (line.startsWith('FØDSELSDATO:')) {
         // convert 26-05-2019 to 2019.05.26
-        resultHorse["birthdate"] = line
-          .substring(12)
-          .split("-")
-          .reverse()
-          .join(".");
-      } else if (line.startsWith("KØN:")) {
-        resultHorse["gender"] = line.substring(4);
-      } else if (line.startsWith("FARVE:")) {
-        resultHorse["color"] = line.substring(6);
-      } else if (line.startsWith("FAR_ID:") || line.includes("F_ID:")) {
-        resultHorse["hid"] = line.split(":")[1];
-      } else if (line.startsWith("MOR_ID:") || line.includes("M_ID:")) {
-        resultHorse["hid"] = line.split(":")[1];
-      } else if (line.startsWith("OPDRÆTTER:")) {
-        resultHorse["breeder"] = line.substring(10);
-      } else if (line.startsWith("NAVN:")) {
-        resultHorse["name"] = line.substring(5);
-      } else if (line.startsWith("ØVRIG_IDENT:")) {
+        resultHorse['birthdate'] = line.substring(12).split('-').reverse().join('.');
+      } else if (line.startsWith('KØN:')) {
+        resultHorse['gender'] = line.substring(4);
+      } else if (line.startsWith('FARVE:')) {
+        resultHorse['color'] = line.substring(6);
+      } else if (line.startsWith('FAR_ID:') || line.includes('F_ID:')) {
+        resultHorse['hid'] = line.split(':')[1];
+      } else if (line.startsWith('MOR_ID:') || line.includes('M_ID:')) {
+        resultHorse['hid'] = line.split(':')[1];
+      } else if (line.startsWith('OPDRÆTTER:')) {
+        resultHorse['breeder'] = line.substring(10);
+      } else if (line.startsWith('NAVN:')) {
+        resultHorse['name'] = line.substring(5);
+      } else if (line.startsWith('ØVRIG_IDENT:')) {
         resultHorse.additionalRegistrationNumbers.push(line.substring(12));
-      } else if (line.startsWith("PRIMÆRE_KÅRINGER:")) {
-        resultHorse["evaluation"] = line.substring(17);
-      } else if (line === "") {
+      } else if (line.startsWith('PRIMÆRE_KÅRINGER:')) {
+        resultHorse['evaluation'] = line.substring(17);
+      } else if (line === '') {
         // pass
       } else {
-        console.debug(
-          `Received unknown key for line "${line}" identity search result with`
-        );
+        console.debug(`Received unknown key for line "${line}" identity search result with`);
       }
     });
-    const sireFilter = filter + "F";
-    const damFilter = filter + "M";
-    resultHorse["sire"] = parseHorse(
+    const sireFilter = filter + 'F';
+    const damFilter = filter + 'M';
+    resultHorse['sire'] = parseHorse(
       horses.filter((block) => block.startsWith(sireFilter)),
       sireFilter
     );
-    resultHorse["dam"] = parseHorse(
+    resultHorse['dam'] = parseHorse(
       horses.filter((block) => block.startsWith(damFilter)),
       damFilter
     );
     return resultHorse;
   };
 
-  const horse = parseHorse(text.split("\n\n"));
+  const horse = parseHorse(text.split('\n\n'));
   return horse;
 };
 
@@ -94,8 +88,8 @@ export const parseIdentitySearchResult = (text) => {
   }
   const results = [];
   let currentResult = null;
-  text.split("\n").forEach((line) => {
-    if (line.startsWith("ID:")) {
+  text.split('\n').forEach((line) => {
+    if (line.startsWith('ID:')) {
       const value = line.substring(3);
       if (currentResult) {
         results.push(currentResult);
@@ -103,23 +97,21 @@ export const parseIdentitySearchResult = (text) => {
       currentResult = {
         hid: value,
       };
-    } else if (line.startsWith("NAVN:")) {
+    } else if (line.startsWith('NAVN:')) {
       const value = line.substring(5);
-      currentResult["name"] = value;
-    } else if (line.startsWith("IDENT:")) {
+      currentResult['name'] = value;
+    } else if (line.startsWith('IDENT:')) {
       const value = line.substring(6);
-      currentResult["registrationNumber"] = value;
-    } else if (line.startsWith("AVLSFORBUND:")) {
+      currentResult['registrationNumber'] = value;
+    } else if (line.startsWith('AVLSFORBUND:')) {
       const value = line.substring(12);
-      currentResult["breedingAssociation"] = value;
-    } else if (line === "") {
+      currentResult['breedingAssociation'] = value;
+    } else if (line === '') {
       // pass
-    } else if (line === "NOT MATCHED") {
+    } else if (line === 'NOT MATCHED') {
       return [];
     } else {
-      console.debug(
-        `Received unknown key for line "${line}" identity search result with`
-      );
+      console.debug(`Received unknown key for line "${line}" identity search result with`);
     }
   });
   if (currentResult) {
@@ -132,7 +124,7 @@ export const insertHorseIntoFilemaker = (registrationNumber) => {
   getParantageByRegistrationNumber(registrationNumber).then((parentage) => {
     console.debug(JSON.stringify(parentage, null, 4));
     if (!parentage) {
-      alert("Kunne ikke finde match i HesteDB");
+      alert('Kunne ikke finde match i HesteDB');
       return;
     }
     const { sire, dam } = parentage;
@@ -142,23 +134,15 @@ export const insertHorseIntoFilemaker = (registrationNumber) => {
       ...parentage,
       sire: {
         ...sire,
-        registrationNumber: sireExistingRegistration
-          ? sireExistingRegistration
-          : sire.registrationNumber,
+        registrationNumber: sireExistingRegistration ? sireExistingRegistration : sire.registrationNumber,
       },
       dam: {
         ...dam,
-        registrationNumber: damExistingRegistration
-          ? damExistingRegistration
-          : dam.registrationNumber,
+        registrationNumber: damExistingRegistration ? damExistingRegistration : dam.registrationNumber,
       },
     };
     // eslint-disable-next-line no-undef
-    FileMaker.PerformScriptWithOption(
-      "IMPORT_FROM_HESTEDB",
-      JSON.stringify(toMatchExistingRecords),
-      "0"
-    );
+    FileMaker.PerformScriptWithOption('IMPORT_FROM_HESTEDB', JSON.stringify(toMatchExistingRecords), '0');
   });
 };
 
@@ -166,7 +150,7 @@ let fuse = null;
 
 export const findBestMatch = (horse) => {
   if (!fuse) {
-    console.debug("Fuse has not been initialized");
+    console.debug('Fuse has not been initialized');
     return null;
   }
   const { name, registrationNumber, additionalRegistrationNumbers } = horse;
@@ -195,43 +179,51 @@ export const findBestMatch = (horse) => {
 };
 
 export const test = (data) => {
-  console.debug("test: parameter", JSON.parse(data));
+  console.debug('test: parameter', JSON.parse(data));
 };
 
 export const populateExistingHorses = (data) => {
-  console.log("Starting population of existing horses");
+  // console.log("Starting population of existing horses");
+  alert('Starting population of existing horses');
   let parsedData = data;
 
-  if (typeof data === "string") {
+  if (typeof data === 'string') {
     parsedData = JSON.parse(data);
     parsedData = parsedData.response.data.map(({ fieldData }) => ({
-      id: fieldData["Patient Record"],
-      name: fieldData["Patient Name"],
+      id: fieldData['Patient Record'],
+      name: fieldData['Patient Name'],
     }));
-    console.log(parsedData);
   }
   const options = {
     includeScore: true,
-    keys: ["id", { name: "name", weight: 10 }],
+    keys: ['id', { name: 'name', weight: 10 }],
     threshold: 0.8,
     useExtendedSearch: true,
     minMatchCharLength: 2,
   };
   fuse = new Fuse(parsedData, options);
-  console.log("Finished populating existing horses");
+  // console.log("Finished populating existing horses");
+  alert('Finished populating existing horses');
 };
 
 window.filemaker = {
   insertHorseIntoFilemaker,
   populateExistingHorses,
+  test,
 };
 
-console.log("Scripts loaded succesfully");
 // Filemaker is not available right away
 setTimeout(() => {
+  console.log('Scripts loaded succesfully');
   if (window.FileMaker) {
-    console.log("Requesting population of existing horse data");
-    // eslint-disable-next-line no-undef
-    FileMaker.PerformScriptWithOption("INDEX_WEBVIEW_SEARCH", "", "0");
+    console.log('Requesting population of existing horse data');
+    try {
+      // eslint-disable-next-line no-undef
+      FileMaker.PerformScriptWithOption('INDEX_WEBVIEW_SEARCH', '', '0');
+    } catch (error) {
+      console.log('error', error);
+    }
+  } else {
+    console.error('FileMaker not found')
   }
-}, 2000);
+}, 10000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: ["whatwg-fetch", "./src/index.js"],
+  mode: 'production',
+  entry: ['whatwg-fetch', './src/index.js'],
   // devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
+        test: /\.m?js$/,
+        // Some module should not be transpiled by Babel
+        // See https://github.com/zloirock/core-js/issues/743#issuecomment-572074215
+        exclude : [
+          /\bcore-js\b/,
+          /\bwebpack\/buildin\b/
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', { useBuiltIns: 'entry', corejs: 3 }]],
+        },
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "FileMaker Script",
-      inlineSource: ".(js|css)$",
+      title: 'FileMaker Script',
+      inlineSource: '.(js|css)$',
     }),
     new ScriptExtHtmlWebpackPlugin({
       inline: [/\.js$/],
     }),
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
